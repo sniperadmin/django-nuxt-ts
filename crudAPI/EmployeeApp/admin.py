3991869090caf54775c59c19c827ff66ca79ed12
Nasr Galal <nasrbue@gmail.com>
2021-08-18 14:20:38 +0200

    feat: register models in admin portal

--- crudAPI/EmployeeApp/admin.py
+++ crudAPI/EmployeeApp/admin.py
@@ -1,4 +1,28 @@
 from django.contrib import admin
-from .models import Department, Employee
+from .models import (
+  Department,
+  Employee,
+  Company,
+  ProductSize,
+  Category,
+  Product,
+  ProductSite,
+  Comment,
+)
+
 # Register your models here.
-admin.site.register([Department, Employee])
+admin.site.register(
+  [
+    Department,
+    Employee,
+    Company,
+    ProductSize,
+    Category,
+    Product,
+    ProductSite,
+    Comment,
+  ]
+)
+
+# Customize header
+admin.site.site_header = 'Basic App'
